37f0fb9a37861d6df6dc2800aae27d8ec586880a
muhamad-anggraito <muhamad.anggraito@gmail.com>
2020-09-26 23:47:23 +0700

    checkpoint 9

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,12 +1,50 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image, StyleSheet} from 'react-native';
 import Card from './Card';
+import CardSection from './CardSection';
 
+const style = StyleSheet.create({
+  productImage: {
+    flex: 1,
+    height: 300,
+  },
+  productName: {
+    flex: 1,
+    fontSize: 18,
+  },
+  productPrice: {
+    width: 100,
+    textAlign: 'right',
+    fontSize: 18,
+    color: 'navy',
+  },
+});
 class ProductItem extends Component {
+  getPriceFormatted(price) {
+    if (price) {
+      const priceFormated = price
+        .toString()
+        .split('')
+        .reverse()
+        .reduce((a, c, i) => c + (i && i % 3 === 0 ? '.' : '') + a, '');
+      return `Rp${priceFormated}`;
+    }
+    return 'Rp0';
+  }
   render() {
+    const {name, image, price, actionLink} = this.props.product;
+
     return (
       <Card>
-        <Text>{this.props.product.name}</Text>
+        <CardSection>
+          <Image style={style.productImage} source={{uri: image}} />
+        </CardSection>
+        <CardSection>
+          <Text style={style.productName}>{name}</Text>
+          <Text style={style.productPrice}>
+            {this.getPriceFormatted(price)}
+          </Text>
+        </CardSection>
       </Card>
     );
   }
